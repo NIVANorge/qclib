073642b169ca53ea5ec196c0c84b0bea1a1b0322
lisapro <elisaveta.pro@gmail.com>
2018-10-15 16:55:27 +0200

    deleted unused modules

--- Exceptions.py
+++
@@ -1,9 +0,0 @@
-
-from Error import Error
-
-class ConfigFileError(Error)    : pass
-class ConfigLoadError(Error)    : pass
-class ConfigPathError(Error)    : pass
-class ConfigTagError(Error)     : pass
-class ConfigIncludeError(Error) : pass
-
