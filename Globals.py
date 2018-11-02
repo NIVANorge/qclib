625b4d6d9502b4ad6f37af6d69252136bbd1bccb
lisapro <elisaveta.pro@gmail.com>
2018-11-02 13:23:15 +0100

    code cleaning in globals, start to work on local testing

--- Globals.py
+++ Globals.py
@@ -7,72 +7,8 @@ pyFerry.Globals
 Provides package access to Global variables
 
 (C) 14. jan. 2016 Pierre Jaccard
-
-import datetime 
-from .Error import Error
-
-FolderMetaFile = 'meta.ini' 
-FolderLogFile  = 'syslog.log'
-
-CATEGORIES = ('provider', 'platform')
-
-MD5_DATA_NAME = 'md5'
-
-D_FORMAT_USR  = '%Y-%m-%d' 
-DT_FORMAT_USR = '%Y-%m-%d %H:%M:%S'
-DT_FORMAT_ISO = '%Y%m%dT%H%M%SZ'
-
-class FileStatus:
-    NEW_RAW_DATA_         = 'NEW_RAW_DATA_'
-    NEW_RAW_DATA_FERRYBOX = 'NEW_RAW_DATA_FERRYBOX'
-    NEW_RAW_DATA_PCO2     = 'NEW_RAW_DATA_PCO2'
-    NEW_NAV_DATA          = 'NEW_NAV_DATA'
-    NEW_RTQC_FLAGS        = 'NEW_RTQC_FLAGS'
-    SSRDV_EXTRACTED       = 'SSRDV_EXTRACTED'
-    JERICO_EXTRACTED      = 'JERICO_EXTRACTED'
-    IOF_EXTRACTED         = 'IOF_EXTRACTED'
-    NEW_PORT_EVENTS       = 'NEW_PORT_EVENTS'
-    NEW_TRANSECTS         = 'NEW_TRANSECTS'
-    
-class Operations:
-    ADD_RAW_DATA_         = 'ADD_RAW_DATA_'
-    ADD_RAW_DATA_FERRYBOX = 'ADD_RAW_DATA_FERRYBOX'
-    ADD_RAW_DATA_PCO2     = 'ADD_RAW_DATA_PCO2'
-    COMBINE_RAW_DATA_     = 'COMBINE_RAW_DATA_' 
-    COMBINE_RAW_DATA_PCO2 = 'COMBINE_RAW_DATA_PCO2' 
-    UPDATE_NAVIGATION     = 'ADD_NAVIGATION'
-    APPLY_RTQC            = 'APPLY_RTQC'
-    EXTRACT_SSRDV         = 'EXTRACT_SSRDV'
-    EXTRACT_JERICO        = 'EXTRACT_JERICO'
-    EXTRACT_IOF           = 'EXTRACT_IndreOsloFjord'
-    SCAN_FOR_PORT_EVENTS  = 'SCAN_FOR_PORT_EVENTS'       
-    SCAN_FOR_TRANSECTS    = 'SCAN_FOR_TRANSECTS'
-    EXTRACT_CMEMS         = 'EXTRACT_CMEMS'
-  
-class EventTypes:
-    UNKNOWN   = 'NA'
-    DEPARTURE = 'DEPARTURE'
-    ARRIVAL   = 'ARRIVAL'
-    IGNORED   = 'IGNORED_EVENT'
-    
-class netCDFGroups:
-    RAW_FERRYBOX = '/raw/ferrybox'
-    
-class SQLTransformations:
-    
-    SCALE = 10000
-    
-    @classmethod
-    def pos2sql(cls, pos):
-        sql = int(round(pos * cls.SCALE))
-        return(sql)
-
-    @classmethod
-    def sql2pos(cls, sql):
-        pos = sql/cls.SCALE
-        return(pos)
-"""       
-
+"""
+   
 class Areas:
 
     Baltic = {
@@ -166,20 +102,3 @@ class Local_Threshold_Ranges:
                 ]
       
     
-'''if False:        
-    POOLCATEGORIES = ('provider', 'platform', 'year')
-    
-    INSTALLATIONS_HEADER = ('ship', 'day', 'hour', 'par:Ed', 'dev:Ed', 
-                            'par:Ld', 'dev:Ld', 'iza:Ld', 'iaa:Ld',
-                            'par:Lu', 'dev:Lu', 'iza:Lu', 'iaa:Lu',
-                            'par:UV', 'dev:UV')
-    
-    class NotAnError(Error): pass
-    
-    VERSIONS_TO_KEEP = 10
-    
-    PI_NAME = 'Kai Sorensen'
-       
-    # MERIS Wavelengths                                                                                                       
-    MERIS_WLEN = [ 412, 443, 490, 510, 560, 620, 665, 681, 709, 753, 778, 865, 885 ]
-    MERIS_TRNG = [ 10-5, 10+5 ]   '''
