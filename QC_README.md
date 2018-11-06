0cef4c8945e277b79e01b5b4c4e26d0da1c730a4
lisapro <elisaveta.pro@gmail.com>
2018-11-06 15:44:48 +0100

    added links to the documentation, unittests from old ferrybox repo

---
+++ QC_README.md
@@ -0,0 +1,18 @@
+# Quality Control tests 
+
+Quality control tests are developed for several NIVA projects  
+Current QC tests module is based on three documents: 
+
+1. CMEMS Quality Control of Biogeochemical Measurements
+http://archimer.ifremer.fr/doc/00251/36232/34792.pdf   
+DOI http://doi.org/10.13155/36232
+
+1. EuroGOOS DATA-MEQ working group (2010). 
+Recommendations for in-situ data Near Real Time Quality Control. https://doi.org/10.13155/36230
+https://archimer.ifremer.fr/doc/00251/36230/ 
+
+1. Catherine Schmechtig, Virginie Thierry and the Bio Argo Team (2015). 
+Argo Quality Control Manual For Biogeochemical Data. http://dx.doi.org/10.13155/40879
+https://archimer.ifremer.fr/doc/00298/40879/42267.pdf
+
+
---
+++ qc_unittest.py
@@ -0,0 +1,48 @@
+'''
+Created on 15. jan. 2018
+
+@author: ELP
+'''
+import QC
+#import json
+import unittest
+import pandas as pd 
+
+
+class FrozenTest(unittest.TestCase):
+    a = {
+       'CHLA_FLUORESCENCE' : [0.345, 10.0, 50,50],
+       'CTD_SALINITY' : [ 25.700, 25.720, 77 , 77],
+       'CTD_TEMPERATURE' : [14.024, 14.014, -20, -20],
+       'GPS_LATITUDE' : [59.9091, 59.9091, 59.9091, 159.9091],
+       'GPS_LONGITUDE' : [10.7057, 10.7077, 10.7087, 70.7087],
+       'SHIP_CODE' : ['FA','FA','FA','FA']
+        }
+    df_a = pd.DataFrame(data=a)
+    
+    
+    def test_frozen(self):
+        ''' FrozenTest should add right flags'''
+        #for var in (self.a,self.b):
+        metadata_a,metadata_list = utils.get_metadata(self.df_a)          
+        #test = qc.QC(df_a)
+        flags,codes,overall_flagstest = utils.call_QC(metadata_a,self.df_a)
+        df_flags = pd.DataFrame(data=flags)
+        for key in self.df_a.keys():
+            if key not in ['GPS_LATITUDE','GPS_LONGITUDE','SHIP_CODE']:
+                self.assertEqual(flags[key]['FROZEN_VALUE'],[1, 1, 1,-1])
+
+
+    def test_global_range(self):
+        ''' GlobalRangeTest should add right flags'''
+        metadata_a,metadata_list = utils.get_metadata(self.df_a)  
+        flags,codes,overall_flagstest = utils.call_QC(metadata_a,self.df_a)       
+        for key in self.df_a.keys(): 
+            if key not in ['GPS_LATITUDE','GPS_LONGITUDE','CHLA_FLUORESCENCE','SHIP_CODE']:            
+                self.assertEqual(flags[key]['GLOBAL_RANGE'],[1,1,-1,-1])
+
+    
+
+            
+if __name__ == '__main__':
+    unittest.main()
