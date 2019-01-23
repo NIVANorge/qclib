051b7564cae0b882cc994997788bb49ed5122bdf
Håkon Drolsum Røkenes <hakon.rokenes@niva.no>
2019-01-23 14:12:06 +0100

    added -L to publish curl and fixed issue with print that did not work

--- publish_package.sh
+++ publish_package.sh
@@ -4,7 +4,4 @@
 VERSION=$(python setup.py --version)
 python setup.py sdist
 
-$file = "dist/qclib-${VERSION}.tar.gz"
-echo "uploading package $file"
-
-curl -i --fail -F package=@$file https://${FURY_TOKEN}@push.fury.io/niva/
+curl -i -L --fail -F package=@dist/qclib-${VERSION}.tar.gz https://${FURY_TOKEN}@push.fury.io/niva/
