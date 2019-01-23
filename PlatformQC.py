189778c23e4349acac0924b8d38777ebb4a51e0d
Zofia Rudjord <zofia.rudjord@niva.no>
2019-01-23 12:57:44 +0100

    Updating keys of common_tests dictionary to match test names in metadata

--- PlatformQC.py
+++ PlatformQC.py
@@ -25,23 +25,23 @@ common_tests = {
 
      '*':
          { 'FROZEN_TEST': [QCTests.RT_frozen_test,{}],
-           'MISSING_VALUE': [QCTests.missing_value_test,{'nan':-999}
+           'MISSING_VALUE_TEST': [QCTests.missing_value_test,{'nan':-999}
                              ]},
      'temperature':
-         { 'GLOBAL_RANGE': [QCTests.range_test, 
+         { 'GLOBAL_RANGE_TEST': [QCTests.range_test,
                             Thresholds.Global_Threshold_Ranges.Temperature]},
      'salinity':
-         { 'GLOBAL_RANGE': [QCTests.range_test, 
+         { 'GLOBAL_RANGE_TEST': [QCTests.range_test,
                             Thresholds.Global_Threshold_Ranges.Salinity]},
      'fluorescence':
-         { 'GLOBAL_RANGE': [QCTests.range_test, 
+         { 'GLOBAL_RANGE_TEST': [QCTests.range_test,
                             Thresholds.Global_Threshold_Ranges.Fluorescence],
-           'LOCAL_RANGE' : [QCTests.range_test, 
+           'LOCAL_RANGE_TEST' : [QCTests.range_test,
                             Thresholds.Local_Threshold_Ranges.Fluorescence]},
      'oxygen_concentration':
-         { 'GLOBAL_RANGE': [QCTests.range_test, 
+         { 'GLOBAL_RANGE_TEST': [QCTests.range_test,
                             Thresholds.Global_Threshold_Ranges.Oxygen],
-           'LOCAL_RANGE' : [QCTests.range_test, 
+           'LOCAL_RANGE_TEST' : [QCTests.range_test,
                             Thresholds.Local_Threshold_Ranges.Oxygen]}}
                             
 class PlatformQC(QCTests):
