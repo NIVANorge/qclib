da3c3e078b406bdeac1ae67d6efc698b2a782b86
Zofia Rudjord <zofia.rudjord@niva.no>
2019-05-08 15:18:40 +0200

    Re-written validate_input module such that data only the part of historical/future data which have gaps is discarded. This solves issue #201 on nivacloud. Updated tests which failed due to changes.

--- qclib/utils/validate_input.py
+++ qclib/utils/validate_input.py
@@ -1,8 +1,7 @@
-import logging
 import pandas as pd
 import datetime
 from .qc_input import QCInput_df
-from .transform_input import merge_data, merge_data_spike
+from .transform_input import merge_data
 
 
 def has_duplicates(df: pd.DataFrame) -> bool:
@@ -10,7 +9,7 @@ def has_duplicates(df: pd.DataFrame) -> bool:
     return duplicates
 
 
-def has_time_reversed(current_data_df: pd.DataFrame, additional_data_df: pd.DataFrame, mode: int) -> bool:
+def has_time_reversed(current_data_df: pd.DataFrame, additional_data_df: pd.DataFrame, mode: int) -> datetime:
     if mode == 1:
         reversed = current_data_df["time"].iloc[0] < additional_data_df["time"].iloc[0]
     else:
@@ -19,65 +18,44 @@ def has_time_reversed(current_data_df: pd.DataFrame, additional_data_df: pd.Data
     return reversed
 
 
-def validate_data_for_time_gaps(df: pd.DataFrame, fuzzy_seconds: int = 0) -> bool:
-    """
-    Validates that the given dataframe has no time gaps between signals.
-
-    if fuzzy_seconds is specified, time gaps lower than fuzzy_seconds will
-    be considered not a time gap
-
-    Assumes a dataframe with a time column and is sorted on time (ascending or decending does not matter)
-    """
-
-    def has_timegaps(dt, first_dt):
-        return abs(dt - first_dt) <= datetime.timedelta(seconds=fuzzy_seconds)
+def remove_data_after_time_gap(df: pd.DataFrame, time_error: int = 0) -> int:
+    def _has_timegaps(dt, first_dt):
+        return abs(dt - first_dt) <= datetime.timedelta(seconds=time_error)
 
     dt_list = df['time'].diff().dropna()
     first_dt = dt_list.iloc[0]
-    no_time_gaps = all(has_timegaps(dt, first_dt) for dt in dt_list)
-    return no_time_gaps
-
-
-def validate_data(qcplatform, current_data_df: pd.DataFrame, additional_data_df: pd.DataFrame, mode: int) -> bool:
-    is_valid = True
-    if additional_data_df is not None and len(additional_data_df) > 1:
-        df = merge_data(current_data_df, additional_data_df)
-        assert has_duplicates(df) == False, "duplicated time stamps in historical data"
-        assert has_time_reversed(current_data_df, additional_data_df, mode) == False, \
-            "historical data are future in time or future data are historical"
-        is_valid = validate_data_for_time_gaps(df, fuzzy_seconds=qcplatform.accept_time_difference)
-    return is_valid
-
-
-def validate_data_for_spike(qcplatform, current_data_df: pd.DataFrame, additional_data_df: pd.DataFrame,
-                            additional_data_df2: pd.DataFrame) -> bool:
-    is_valid = True
-    if additional_data_df is not None and additional_data_df2 is not None and \
-            len(additional_data_df) == 1 and len(additional_data_df2) == 1:
-        df = merge_data_spike(current_data_df, additional_data_df, additional_data_df2)
-        assert has_duplicates(df) == False, "duplicated time stamps in historical data"
-        is_valid = validate_data_for_time_gaps(df, fuzzy_seconds=qcplatform.accept_time_difference)
-    return is_valid
+    no_time_gaps = [_has_timegaps(dt, first_dt) for dt in dt_list]
+    if not all(no_time_gaps):
+        gen = (i for i, v in enumerate(no_time_gaps) if v is False)
+        first_gap_index = next(gen)
+        df.drop(df.index[first_gap_index + 1:], inplace=True)
+    return first_dt
 
 
 def validate_additional_data(qcplatform, qcinput: QCInput_df):
-    """This function checks validity of additional data
-    if additional data have time gaps, they are reset to empty"""
-
-    is_valid = True
-    if not validate_data(qcplatform, qcinput.current_data, qcinput.historical_data, 1):
-        is_valid = False
-        qcinput.historical_data = pd.DataFrame.from_dict({})
-        logging.debug("Removing historical data due to time gaps")
-
-    if not validate_data(qcplatform, qcinput.current_data, qcinput.future_data, 2):
-        is_valid = False
-        qcinput.future_data = pd.DataFrame.from_dict({})
-        logging.debug("Removing future data due to time gaps")
-
-    if not validate_data_for_spike(qcplatform, qcinput.current_data, qcinput.historical_data, qcinput.future_data):
-        qcinput.future_data = pd.DataFrame.from_dict({})
-        qcinput.historical_data = pd.DataFrame.from_dict({})
-        logging.debug("Removing additional data due to time gaps")
-
-    return is_valid
+    # Validate historical data
+    if qcinput.historical_data is not None:
+        df = merge_data(qcinput.current_data, qcinput.historical_data)
+        assert has_duplicates(df) == False, "duplicated time stamps in historical data"
+        assert has_time_reversed(qcinput.current_data, qcinput.historical_data, 1) == False, \
+            "historical data are future in time"
+        sampling_int_hist = remove_data_after_time_gap(df, time_error=qcplatform.accept_time_difference)
+        df.drop(df.index[[-1]], inplace=True)
+        qcinput.historical_data = df
+    # Validate future data
+    if qcinput.future_data is not None:
+        df = merge_data(qcinput.current_data, qcinput.future_data)
+        assert has_duplicates(df) == False, "duplicated time stamps in historical data"
+        assert has_time_reversed(qcinput.current_data, qcinput.future_data, 2) == False, \
+            "future data are historical in time"
+        sampling_int_future = remove_data_after_time_gap(df, time_error=qcplatform.accept_time_difference)
+        df.drop(df.index[[0]], inplace=True)
+        qcinput.future_data = df
+    # Special case: we have just 1 historical and 1 future samples, we need to check weather
+    # data are valid for spike test
+    if qcinput.future_data is not None and len(qcinput.future_data) == 1 and \
+            qcinput.historical_data is not None and len(qcinput.historical_data) == 1:
+        if abs(sampling_int_hist - sampling_int_future) \
+                > datetime.timedelta(seconds=qcplatform.accept_time_difference):
+            qcinput.future_data = pd.DataFrame.from_dict({})
+            qcinput.historical_data = pd.DataFrame.from_dict({})
