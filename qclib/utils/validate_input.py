8c9e75c55449f32863d0bf0908bec9a996184d21
Zofia Rudjord <zofia.rudjord@niva.no>
2019-05-23 16:00:42 +0200

    1. Fixing type of qctests passed to QC.execute 2. Bugfix to is_sorted: list cannot be empty 3. Updating version to 2.0.1

--- qclib/utils/validate_input.py
+++ qclib/utils/validate_input.py
@@ -30,6 +30,6 @@ def validate_data_for_frozen_test(data: QCInput, size) -> List[bool]:
 
 def is_sorted(data: QCInput) -> bool:
     is_valid = data.values[0] <= data.values[-1]
-    if data.locations is not None:
+    if data.locations is not None and len(data.locations):
         is_valid &= data.locations[0] <= data.locations[-1]
     return is_valid
