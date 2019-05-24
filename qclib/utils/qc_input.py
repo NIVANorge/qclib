85a8c17be41f54a0a4cfc3e86bfd85331f41ec3c
Zofia Rudjord <zofia.rudjord@niva.no>
2019-05-24 13:24:21 +0200

    Updating measurement model and type checking in QCInput model

--- qclib/utils/qc_input.py
+++ qclib/utils/qc_input.py
@@ -1,7 +1,10 @@
 from typing import List, Optional
+
 from pydantic import BaseModel
+
+from qclib.utils.measurement import Measurement, Location
 
 
 class QCInput(BaseModel):
-    values: List
-    locations: Optional[List]
+    values: List[Measurement]
+    locations: Optional[List[Location]]
