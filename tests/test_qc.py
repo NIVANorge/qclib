72a71e2a3bc9d923e1c1a1827d80c3f41b66a50f
Zofia Rudjord <zofia.rudjord@niva.no>
2019-06-05 16:02:48 +0200

    Adding testdata and tests for local_range and final flag

--- tests/test_qc.py
+++ tests/test_qc.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 import qclib.utils.Thresholds
 from qclib import QC
 from qclib.utils.qc_input import QCInput
+from qclib.PlatformQC import PlatformQC
 
 platform_code = 'TF'
 common_tests = QC.init(platform_code).qc_tests
@@ -15,7 +16,7 @@ d = timedelta(seconds=60)
 
 
 def make_toy_data(size):
-    values = [[base_time + d * i, i] for i in range(0, size)]
+    values = [(base_time + d * i, i) for i in range(0, size)]
     location = [(base_time + d * i, 10.708 + i * 0.01, 61 + i * 0.01) for i in range(0, size)]
     return QCInput(values=values, locations=location)
 
@@ -26,27 +27,42 @@ def read_testdata(filename):
         return list(csv_reader)[1:]
 
 
+def write_testdata(filename, header, data):
+    with open(filename, mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file, delimiter='\t')
+        csv_writer.writerow(header)
+        csv_writer.writerows(data)
+
+
 class Tests(unittest.TestCase):
 
     def test_qc_logic(self):
+
         origin_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "testdata")
         filename = os.path.join(origin_dir, "ferrybox_data.csv")
         input_data = read_testdata(filename)
-        values = [[datetime.strptime(item[0].split('.')[0], '%Y-%m-%dT%H:%M:%S'), float(item[1])] for item in
+
+        values = [(datetime.strptime(item[0].split('.')[0], '%Y-%m-%dT%H:%M:%S'), float(item[1])) for item in
                   input_data]
+        locations = [(datetime.strptime(item[0].split('.')[0], '%Y-%m-%dT%H:%M:%S'), float(item[5]), float(item[6]))
+                     for item in input_data]
+
         ref_frozen_test = [int(item[2]) for item in input_data]
         ref_global_range_test = [int(item[3]) for item in input_data]
         ref_argo_spike_test = [int(item[4]) for item in input_data]
-        # locations = [[datetime.strptime(item[0].split('.')[0], '%Y-%m-%dT%H:%M:%S'), 10+0.01*i, 59.9+0.01*i]
-        #              for i, item in enumerate(input_data)]
-        locations = None
+        ref_local_range_test = [int(item[7]) for item in input_data]
+        ref_final_flag = [int(item[8]) for item in input_data]
         flags = QC.execute(qclib.QC.init(platform_code), QCInput(values=values, locations=locations),
                            tests={"salinity": {"global_range_test": False, "frozen_test": False,
-                                               "argo_spike_test": False}})
+                                               "argo_spike_test": False, "local_range_test": False}})
+
+        final_flag = PlatformQC.get_overall_flag(flags)
 
         assert flags["frozen_test"] == ref_frozen_test, "Frozen test failed"
         assert flags["global_range_test"] == ref_global_range_test, "Global range test failed"
         assert flags["argo_spike_test"] == ref_argo_spike_test, "Argo spike test failed"
+        assert flags["local_range_test"] == ref_local_range_test, "Local range test failed"
+        assert final_flag == ref_final_flag, "Final flag calculation failed"
 
     def test_execution_time_with_toy_data(self):
         data = make_toy_data(1500)
@@ -54,7 +70,7 @@ class Tests(unittest.TestCase):
         start = time.time()
         QC.execute(qclib.QC.init(platform_code), data, tests)
         end = time.time()
-        assert (end - start) < 0.2, "Execution time on 1500 is slow"
+        assert (end - start) < 0.2, "Execution time on 1500 signals in the list takes more than 0.2 s."
 
 
 if __name__ == '__main__':
