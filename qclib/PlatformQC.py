67ab8f01fc078f7b6fb7ed122b4bfe5c19d3fdc0
RmStorm <roaldstorm@gmail.com>
2019-10-09 16:51:41 +0200

    Changed how the overall flag calculation handles extra_flags
    It should check for every argument if it's a list or None, Then if it's a list
    it should be used in the calculation if it's None it should be ignored.
    
    Also changed the velocity_from_location_list to be stricter. Location now has
    to be present, the individual lon and lat can still be None.

--- qclib/PlatformQC.py
+++ qclib/PlatformQC.py
@@ -95,26 +95,24 @@ class PlatformQC(QCTests):
         return flags
 
     @staticmethod
-    def get_overall_flag(flags: Dict[str, List[int]], *extra_flags) -> List[int]:
+    def get_overall_flag(flags: Dict[str, List[int]], *extra_flag_lists) -> List[int]:
         # check if None values appear consistently for all flags for a given measurement
-        flags_list = list(flags.values())
-        flags_list_T = np.array(flags_list).T
-        flag_None = np.any(flags_list_T == None, axis=1)
-        assert all(flag_None == np.all(flags_list_T == None, axis=1))
-        # I'm not sure if extra_flags is None or a tuple with None in it
-        # check if flags and extra flags are equal length
-        if extra_flags not in [None, (None,)]:
-            for flags in extra_flags:
-                assert len(flags) == len(flags_list[0])
-                flags_list.append(flags)
-        flags_list_T = np.array(flags_list).T
-        flag_0 = np.any(flags_list_T == 0, axis=1)
-        flag_1 = np.any(flags_list_T == -1, axis=1)
-        flag_None = np.any(flags_list_T == None, axis=1)
-        overall_flag = np.ones(len(flags_list_T))
+        list_of_flags_lists = list(flags.values())
+        for extra_flag_list in extra_flag_lists:
+            if extra_flag_list is not None:
+                assert len(extra_flag_list) == len(list_of_flags_lists[0])
+                list_of_flags_lists.append(extra_flag_list)
+        list_of_flags_lists_T = np.array(list_of_flags_lists).T
+
+        flag_0 = np.any(list_of_flags_lists_T == 0, axis=1)
+        flag_1 = np.any(list_of_flags_lists_T == -1, axis=1)
+        flag_None = np.all(list_of_flags_lists_T == None, axis=1)
+
+        overall_flag = np.ones(len(list_of_flags_lists_T))
         overall_flag[flag_0] = 0
         overall_flag[flag_1] = -1
         overall_flag[flag_None] = None
+
         final_flag = [flag if flag in [-1, 0, 1] else None for flag in overall_flag.tolist()]
         return final_flag
 
